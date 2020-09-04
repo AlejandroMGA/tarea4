81b6d1eda9f93fcf6c54838dfad9c9cd1c95f45d
“AlejandroMGA” <alejandro.amga15@gmail.com>
2020-09-04 16:17:09 -0400

    versiones preliminares

---
+++ diccionario.py
@@ -0,0 +1,6 @@
+#Diccionario
+diccionario = dict(nombre=['Juan', 'Camilo', 'Lucas', 'Valeria'], apellido=['Zamorano', 'Flores', 'Vega', 'Roldán'], 
+nota_programación=['5.5', '6.9', '6.5', '6.0'], nota_física=['5.0', '7.0', '6.0', '5.5'])
+
+#Enunciado (Prueba para ver cómo hacer que el comando diccionario solo escoja un nombre de la lista de nombres)
+print (diccionario['nombre'][0])
---
+++ operaciones.py
@@ -0,0 +1,9 @@
+#Variables
+x = 8
+y = 4
+
+#Operaciones
+print (f'Si x vale {x} e y vale {y}:')
+print (f'La suma de x + y, o sea {x} + {y}, es igual a {x + y}')
+print (f'La resta de x - y, o sea {x} - {y}, es igual a {x - y}')
+ 
