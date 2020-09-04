64ce6c0b4f7b5c752e49e1f33b8261097a0be732
“AlejandroMGA” <alejandro.amga15@gmail.com>
2020-09-04 16:53:01 -0400

    versiones finales

--- diccionario.py
+++ diccionario.py
@@ -2,5 +2,9 @@
 diccionario = dict(nombre=['Juan', 'Camilo', 'Lucas', 'Valeria'], apellido=['Zamorano', 'Flores', 'Vega', 'Roldán'], 
 nota_programación=['5.5', '6.9', '6.5', '6.0'], nota_física=['5.0', '7.0', '6.0', '5.5'])
 
-#Enunciado (Prueba para ver cómo hacer que el comando diccionario solo escoja un nombre de la lista de nombres)
-print (diccionario['nombre'][0])
+#Enunciado (Ahora que se sabe cómo funciona el comando diccionario en listas, se escribe el enunciado entero)
+print (f"El alumno/a {diccionario['nombre'][0]} {diccionario['apellido'][0]} tiene nota {diccionario['nota_programación'][0]} en el ramo de Programación, mientras que en Física su nota es {diccionario['nota_física'][0]}")
+
+#Me va a tener que disculpar por el hecho de que el comando esté muy largo hacia los lados, intenté dejarlo dividido, 
+#pero el comando ya no funcionaba si intentaba hacer uso del "\" como usted dijo en la clase pasada
+
--- operaciones.py
+++ operaciones.py
@@ -6,4 +6,6 @@ y = 4
 print (f'Si x vale {x} e y vale {y}:')
 print (f'La suma de x + y, o sea {x} + {y}, es igual a {x + y}')
 print (f'La resta de x - y, o sea {x} - {y}, es igual a {x - y}')
- 
+print (f'La multiplicación de x * y, o sea {x} * {y}, es igual a {x * y}')
+print (f'La división de x / y, o sea {x} * {y}, es igual a {x / y}')
+print (f'El módulo de x % y, que en otras palabras sería el resto de la división entre x e y, o sea {x} % {y}, es igual a {x % y}')
